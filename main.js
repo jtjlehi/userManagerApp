ec68b73daedeaf94f3aa514c19fd2e408f7bb48f
jared <jared.jacobson.2000@gmail.com>
2018-03-28 10:09:16 -0600

    installed, required, and used bodyParser

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));

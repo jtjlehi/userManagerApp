6682300b0e521654521904dc1a43bcfcab970c6b
jared <jared.jacobson.2000@gmail.com>
2018-04-12 16:31:10 -0600

    made a post call to add append the user data to the users-data.txt file

--- add_post.js
+++ add_post.js
@@ -1,6 +1,16 @@
+const fs = require('fs');
+
 module.exports = function(app) {
     app.post('/add', (req, res) => {
-        console.log(req.body);
+        const body = {
+            name: req.body.name,
+            email: req.body.email,
+            age: req.body.age
+        };
+        const newUser = JSON.stringify(body);
+        fs.appendFile('./users-data.txt', `${newUser}\n`, (err) => {
+            if (err) throw err;
+        });
         res.end('User added');
     });
 }

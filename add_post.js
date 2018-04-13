4240574a16d6f04a6a4aadf93b8f9a1fc8d93619
jared <jared.jacobson.2000@gmail.com>
2018-04-12 20:24:48 -0600

    changed the code so the dataManager added users, and made'em more robust

--- add_post.js
+++ add_post.js
@@ -1,16 +1,10 @@
 const fs = require('fs');
+const DataManager = require('./data_manager');
 
 module.exports = function(app) {
     app.post('/add', (req, res) => {
-        const body = {
-            name: req.body.name,
-            email: req.body.email,
-            age: req.body.age
-        };
-        const newUser = JSON.stringify(body);
-        fs.appendFile('./users-data.txt', `${newUser}\n`, (err) => {
-            if (err) throw err;
-        });
-        res.end('User added');
+        const user = DataManager.generateUser(req.body.name, req.body.string, req.body.age);
+        DataManager.addUser(user)
+        .then(res.end('User added'));
     });
 }
--- data_manager.js
+++ data_manager.js
@@ -1,14 +1,61 @@
 const fs = require('fs');
 
 class DataManager {
-    parse(cbk) {
-        fs.readFile('users-data.txt', {encoding: 'utf8'}, (err, data) => {
-            if (err) throw err;
-            const users = data.split('\n')
-                .filter(userString => userString !== "")
-                .map(userString => JSON.parse(userString));
-            cbk(users);
-        });
+    parse() {
+        return new Promise((resolve, reject) => {
+            fs.readFile('users-data.txt', {encoding: 'utf8'}, (err, data) => {
+                if (err) throw err;
+                const users = data.split('\n')
+                    .filter(userString => userString !== "")
+                    .map(userString => JSON.parse(userString));
+                resolve(users);
+            });
+        })
+    }
+    addUser(user) {
+        return this.parse()
+        .then((users) => {
+            if (users.filter(existingUser => existingUser.id === user.id).length !== 0) {
+                throw new Error('user exists');
+            }
+            console.log(user);
+            fs.appendFile('./users-data.txt', JSON.stringify(user), (err) => {
+                if (err) throw err;
+            });
+        })
+        .catch((err) => {throw err})
+    }
+    generateUser(name, email, age) {
+        const id = this.idGenerator(name + email + age);
+        return {
+            id: id,
+            name: {
+                name: 'name',
+                type: 'text',
+                value: name
+            },
+            email: {
+                name: 'email',
+                type: 'email',
+                value: email
+            },
+            age: {
+                name: 'age',
+                type: 'number',
+                value: age
+            }
+        }
+    }
+    idGenerator(string) {
+        const length = string.length;
+        let hash = 0;
+        if (length === 0) return hash;
+        for (let i = 0; i < length; i ++) {
+            let char = string.charCodeAt(i);
+            hash = ((hash<<5)-hash)+char;
+            hash = hash & hash;
+        }
+        return hash;
     }
 }
 

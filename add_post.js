fca2af44559c4124fc46b06ad9e7bc49bb6d4f7f
jared <jared.jacobson.2000@gmail.com>
2018-04-14 12:26:52 -0600

    fixed a bug that wouldn't send the user email on add user

--- add_post.js
+++ add_post.js
@@ -2,11 +2,11 @@ const DataManager = require('./data_manager');
 
 module.exports = function(app) {
     app.post('/add', (req, res) => {
-        const user = DataManager.generateUser(req.body.name, req.body.string, req.body.age);
+        const user = DataManager.generateUser(req.body.name, req.body.email, req.body.age);
         DataManager.addUser(user)
         .then((err) => {
-            if (err) res.end(err.message)
-            res.end('User added')
+            if (err) res.end(err.message);
+            res.end('User added');
         })
     });
 }

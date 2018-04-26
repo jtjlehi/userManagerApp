ccb3d7c96b2dee852a6257e86037c0d7d7e79f62
jared <jared.jacobson.2000@gmail.com>
2018-04-25 22:13:20 -0600

    fixed the rest of the bugs homie

--- data_manager.js
+++ data_manager.js
@@ -6,7 +6,6 @@ class DataManager {
             fs.readFile('users-data.txt', {encoding: 'utf8'}, (err, data) => {
                 if (err) throw err;
                 const users = JSON.parse(data);
-                console.log(users);
                 resolve(users);
             });
         })
@@ -14,10 +13,20 @@ class DataManager {
     addUser(user) {
         return this.parse()
         .then((users) => {
-            if (users.filter(existingUser => existingUser.userData.id === user.id).length !== 0) {
+            if (users.filter(existingUser => existingUser.id === user.id).length !== 0) {
                 return new Error('Whoops! That user already exists.');
             }
-            fs.appendFile('./users-data.txt', `${JSON.stringify(user)}\n`, (err) => {
+            users.push(user);
+            return users;
+        })
+        .then(users => new Promise((resolve, reject) => {
+            fs.truncate('./users-data.txt', (err) => {
+                if (err) reject(err);
+                resolve(users);
+            });
+        }))
+        .then((users) => {
+            fs.writeFile('./users-data.txt', JSON.stringify(users), (err) => {
                 if (err) return new Error('Sorry, Problem occurred while saving data, we will do everything we can to fix it.');
                 return 'User added';
             });
@@ -45,47 +54,32 @@ class DataManager {
     }
     replaceUser(userId, newUser) {
         const filePath = './users-data.txt';
-
-        fs.readFile(filePath, {encoding: 'utf8'}, (err, data) => {
-            
-        })
-
+        const userID = userId;
         return this.parse()
-        // .then(users => {
-        //     const user = users.find(user => user.userData.id == userId);
-        //     if (user === undefined) throw new Error('User undefined');
-        //     return {start: user.index.start, end: user.index.end};
-        // })
-        // .then((indexes) => {
-        //     return new Promise((resolve, reject) => {
-        //         fs.readFile(filePath, {encoding: 'utf8'}, (err, data) => {
-        //             if (err) reject(err);
-        //             resolve({
-        //                 beforeUser: data.substring(0, indexes.start + 1),
-        //                 afterUser: data.substring(indexes.end + 2)
-        //             });
-        //         });
-        //     })
-        // })
-        // .then((file) => {
-        //     console.log(`file before user: '${file.beforeUser}'`);
-        //     console.log(`file after user: '${file.afterUser}'`);
-        //     const newFile = file.beforeUser + newUser + file.afterUser;
-        //     return new Promise((resolve, reject) => {
-        //         fs.truncate(filePath, (err) => {
-        //             if (err) reject(err);
-        //             resolve(newFile);
-        //         });
-        //     });
-        // })
-        // .then(newFile => {
-        //     return new Promise((resolve, reject) => {
-        //         fs.writeFile(filePath, newFile, (err) => {
-        //             if (err) reject(err);
-        //             resolve();
-        //         });
-        //     });
-        // });
+        // replace the user in the array
+        .then(users => {
+            const userIndex = users.findIndex((user) => user.id == userID);
+            if (userIndex === -1) throw new Error('User not found');
+            if(newUser === '') users.splice(userIndex, 1);
+            else users.splice(userIndex, 1, newUser);
+            return users;
+        })
+        // clear the file
+        .then((users) => {
+            return new Promise((resolve, reject) => {
+                fs.truncate(filePath, (err) => {
+                    if (err) reject(err);
+                    resolve(users);
+                });
+            });
+        })
+        .then(users => new Promise((resolve, reject) => {
+            fs.writeFile(filePath, JSON.stringify(users), {encoding: 'utf8'}, (err) => {
+                if (err) reject(new Error('error writing file'));
+                console.log('users written', users);
+                resolve();
+            })
+        }))
     }
 }
 
--- edit_post.js
+++ edit_post.js
@@ -3,9 +3,13 @@ const dataManager = require('./data_manager');
 module.exports = function(app) {
     app.post('/edit/:user_id', (req, res) => {
         const updatedUser = dataManager.generateUser(req.body.name, req.body.email, req.body.age);
-        dataManager.replaceUser(req.params.user_id, `\n${JSON.stringify(updatedUser)}`)
-        .then(res.render('message', {message: 'User edited'}))
+        dataManager.replaceUser(req.params.user_id, updatedUser)
+        .then(() => {
+            console.log('inside the .then');
+            res.render('message', {message: 'User edited'})
+        })
         .catch(err => {
+            console.log(err);
             res.render('message', {message: err.message !== undefined ? err.message : err})
         });
     });
